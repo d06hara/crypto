a70167aefa6c843d849fb7514d5a30e567b9cfe7
hara <d06.harada.prg@gmail.com>
2020-02-15 16:04:36 +0900

    tweet テーブル作成

--- database/migrations/2020_02_15_155314_create_tweets_table.php
+++ database/migrations/2020_02_15_155314_create_tweets_table.php
@@ -15,6 +15,13 @@ class CreateTweetsTable extends Migration
     {
         Schema::create('tweets', function (Blueprint $table) {
             $table->bigIncrements('id');
+            $table->integer('tweet_id')->nullable()->comment('ツイートID');
+            $table->string('text')->nullable()->comment('ツイート内容');
+            $table->dateTime('tweet_created_at')->nullable()->comment('ツイート日時');
+            $table->string('screen_name')->nullable()->comment('@スクリーンネーム');
+            $table->string('user_name')->nullable()->comment('ユーザー名');
+            $table->integer('bland_id')->nullable();
+            $table->boolean('delete_flg')->nullable();
             $table->timestamps();
         });
     }
