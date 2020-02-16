a62f4506b3cbb016d702feee6eae120d11eba6c5
hara <d06.harada.prg@gmail.com>
2020-02-16 11:01:34 +0900

    tweetモデル名変更

--- app/Console/Commands/GetTweetCommand.php
+++ app/Console/Commands/GetTweetCommand.php
@@ -3,7 +3,7 @@
 namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
-use App\Http\Tweets;
+use App\Http\Tweet;
 
 class GetTweetCommand extends Command
 {
@@ -12,7 +12,7 @@ class GetTweetCommand extends Command
      *
      * @var string
      */
-    protected $signature = 'tweet:get';
+    protected $signature = 'get:tweet';
 
     /**
      * The console command description.
@@ -39,5 +39,19 @@ class GetTweetCommand extends Command
     public function handle()
     {
         //cronで実行したい処理を記述
+        // 検索したいキーワード
+        // とりあえずビットコインでモデル作成
+        $search_key = "ビットコイン";
+        $query = Tweet::get();
+
+        //初期データ
+        if (!is_object($query)) {
+            //DBから最大値を取得
+            $max = $query->max('tweet_id');
+            //tweet_idの最大値から+1してDBのデータと被らないようにする
+            $since_id = $max + 1;
+        } else {
+            $since_id = null;
+        }
     }
 }
