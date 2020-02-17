397ef54b275bd9b4263213b895978a97afb3f4d8
hara <d06.harada.prg@gmail.com>
2020-02-17 23:37:02 +0900

    ベアラートークンを使ってapponly認証でツイートを取得できた

--- app/Console/Commands/GetTweetAppAuthCommand.php
+++ app/Console/Commands/GetTweetAppAuthCommand.php
@@ -47,17 +47,17 @@ class GetTweetAppAuthCommand extends Command
         $key = $config['api_key'];
         $secret_key = $config['secret_key'];
 
-        $query = Tweet::get();
-        dd($query);
-        //初期データ
-        if (!is_object($query)) {
-            //DBから最大値を取得
-            $max = $query->max('tweet_id');
-            //tweet_idの最大値から+1してDBのデータと被らないようにする
-            $since_id = $max + 1;
-        } else {
-            $since_id = null;
-        }
+        // $query = Tweet::get();
+        // dd($query);
+        // //初期データ
+        // if (!is_object($query)) {
+        //     //DBから最大値を取得
+        //     $max = $query->max('tweet_id');
+        //     //tweet_idの最大値から+1してDBのデータと被らないようにする
+        //     $since_id = $max + 1;
+        // } else {
+        //     $since_id = null;
+        // }
 
         // インスタンス作成
         $connection = new Tweet($key, $secret_key);
@@ -67,56 +67,56 @@ class GetTweetAppAuthCommand extends Command
         // 日本で取り扱っている有名銘柄10種
         // それぞれbland_idと対応させる
         // 銘柄を追加する場合はここに追加
-        $search_key_array = array(
-            1 => "ビットコイン OR Bitcoin OR BTC",
-            2 => "ビットコインキャッシュ OR BitcoinCash OR BCH",
-            3 => "イーサリアム OR Ethereum OR ETH",
-            4 => "イーサリアムクラシック OR EthreumClassic OR ETC",
-            5 => "リップル OR Ripple OR XRP",
-            6 => "ライトコイン OR LiteCoin OR LTC",
-            7 => "ネム OR NEM OR XEM",
-            8 => "モナコイン OR  MonaCoin OR MONA",
-            9 => "リスク OR Lisk OR LSK",
-            10 => "ファクトム OR Factom OR FCT",
-        );
-
-        foreach ($search_key_array as $bland_id => $search_key) {
-
-
-
-            // パラメータ
-            $params = array(
-                // "q" => "BTC",
-                "q" => $search_key,
-                "count" => 1,
-                "lang" => "ja",
-                "since_id" => $since_id
-            );
-
-            $tweet_obj = $connection->searchTweet('search/tweets', $params);
-
-            //TwitterAPIからデータが返ってきているか確認
-            if (is_object($tweet_obj)) {
-                //念の為ツイートデータが入ってるか確認
-                if (isset($tweet_obj->statuses)) {
-                    //ツイート保存する処理
-                    $twitter_store = Tweet::tweetStore($tweet_obj, $bland_id);
-                    // $twitter_store = Tweet::tweetStore($twitter_api, $bland_id);
-                }
-            }
-            // }
-
-            dd($tweet_obj);
-        }
-
-        // $params = array(
-        //     'q' => '焼肉',
-        //     'count' => 1,
+        // $search_key_array = array(
+        //     1 => "ビットコイン OR Bitcoin OR BTC",
+        //     2 => "ビットコインキャッシュ OR BitcoinCash OR BCH",
+        //     3 => "イーサリアム OR Ethereum OR ETH",
+        //     4 => "イーサリアムクラシック OR EthreumClassic OR ETC",
+        //     5 => "リップル OR Ripple OR XRP",
+        //     6 => "ライトコイン OR LiteCoin OR LTC",
+        //     7 => "ネム OR NEM OR XEM",
+        //     8 => "モナコイン OR  MonaCoin OR MONA",
+        //     9 => "リスク OR Lisk OR LSK",
+        //     10 => "ファクトム OR Factom OR FCT",
         // );
 
+        // foreach ($search_key_array as $bland_id => $search_key) {
 
 
-        // $tweet_obj = $connection->searchTweet('search/tweets', $params);
+
+        //     // パラメータ
+        //     $params = array(
+        //         // "q" => "BTC",
+        //         "q" => $search_key,
+        //         "count" => 1,
+        //         "lang" => "ja",
+        //         "since_id" => $since_id
+        //     );
+
+        //     $tweet_obj = $connection->searchTweet('search/tweets', $params);
+
+        //     //TwitterAPIからデータが返ってきているか確認
+        //     if (is_object($tweet_obj)) {
+        //         //念の為ツイートデータが入ってるか確認
+        //         if (isset($tweet_obj->statuses)) {
+        //             //ツイート保存する処理
+        //             $twitter_store = Tweet::tweetStore($tweet_obj, $bland_id);
+        //             // $twitter_store = Tweet::tweetStore($twitter_api, $bland_id);
+        //         }
+        //     }
+        //     // }
+
+        //     dd($tweet_obj);
+        // }
+
+        $params = array(
+            'q' => '焼肉',
+            'count' => 1,
+        );
+
+
+
+        $tweet_obj = $connection->searchTweet('search/tweets', $params);
 
         $tweets_arr = json_decode($tweet_obj);
 
