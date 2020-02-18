2bedf6c95b072dc15b03fe7f74cb450ac3be5b2a
hara <d06.harada.prg@gmail.com>
2020-02-18 23:21:13 +0900

    自作認証クラスに記述

--- app/Tweet.php
+++ app/Tweet.php
@@ -9,56 +9,56 @@ class Tweet extends Model
 
     // app authお試し
     // --------------------------------------------
-    private $bearer_token;
+    // private $bearer_token;
 
-    public function __construct($consumer_key, $consumer_secret)
-    {
-        $this->_bearer_token = $this->_getBearerToken($consumer_key, $consumer_secret);
-    }
+    // public function __construct($consumer_key, $consumer_secret)
+    // {
+    //     $this->_bearer_token = $this->_getBearerToken($consumer_key, $consumer_secret);
+    // }
 
-    private function _getBearerToken($consumer_key, $consumer_secret)
-    {
-        $oauth2_url = 'https://api.twitter.com/oauth2/token';
+    // private function _getBearerToken($consumer_key, $consumer_secret)
+    // {
+    //     $oauth2_url = 'https://api.twitter.com/oauth2/token';
 
-        $token = base64_encode(urlencode($consumer_key) . ':' . urlencode($consumer_secret));
+    //     $token = base64_encode(urlencode($consumer_key) . ':' . urlencode($consumer_secret));
 
-        $request = array(
-            'grant_type' => 'client_credentials'
-        );
+    //     $request = array(
+    //         'grant_type' => 'client_credentials'
+    //     );
 
-        $opts['http'] = array(
-            'method'    => 'POST',
-            'header'    => 'Content-type: application/x-www-form-urlencoded;charset=UTF-8' . "\r\n"
-                . 'Authorization: Basic ' . $token,
-            'content'   => http_build_query($request, '', '&', PHP_QUERY_RFC3986)
-        );
+    //     $opts['http'] = array(
+    //         'method'    => 'POST',
+    //         'header'    => 'Content-type: application/x-www-form-urlencoded;charset=UTF-8' . "\r\n"
+    //             . 'Authorization: Basic ' . $token,
+    //         'content'   => http_build_query($request, '', '&', PHP_QUERY_RFC3986)
+    //     );
 
-        $context = stream_context_create($opts);
+    //     $context = stream_context_create($opts);
 
-        $response_json = file_get_contents($oauth2_url, false, $context);
+    //     $response_json = file_get_contents($oauth2_url, false, $context);
 
-        $response_arr = json_decode($response_json, true);
+    //     $response_arr = json_decode($response_json, true);
 
-        return $response_arr['access_token'];
-    }
+    //     return $response_arr['access_token'];
+    // }
 
-    public function searchTweet($api, $params = array())
-    {
-        $api_url = 'https://api.twitter.com/1.1/' . $api . '.json';
+    // public function searchTweet($api, $params = array())
+    // {
+    //     $api_url = 'https://api.twitter.com/1.1/' . $api . '.json';
 
-        if ($params) {
-            $request = http_build_query($params, '', '&', PHP_QUERY_RFC3986);
-            $api_url .= '?' . $request;
-        }
+    //     if ($params) {
+    //         $request = http_build_query($params, '', '&', PHP_QUERY_RFC3986);
+    //         $api_url .= '?' . $request;
+    //     }
 
-        $opts['http'] = array(
-            'header' => 'Authorization: Bearer ' . $this->_bearer_token
-        );
+    //     $opts['http'] = array(
+    //         'header' => 'Authorization: Bearer ' . $this->_bearer_token
+    //     );
 
-        $context = stream_context_create($opts);
+    //     $context = stream_context_create($opts);
 
-        return file_get_contents($api_url, false, $context);
-    }
+    //     return file_get_contents($api_url, false, $context);
+    // }
     // ---------------------------------------------
 
 
