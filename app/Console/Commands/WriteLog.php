34b30d24226b2edb76794f7110b556843fc51d7b
hara <d06.harada.prg@gmail.com>
2020-03-11 00:28:15 +0900

    ユーザーごとに自動フォロー

--- app/Console/Commands/WriteLog.php
+++ app/Console/Commands/WriteLog.php
@@ -2,6 +2,7 @@
 
 namespace App\Console\Commands;
 
+use Abraham\TwitterOAuth\TwitterOAuth;
 use Illuminate\Console\Command;
 use App\Models\User;
 use App\Models\TwitterAccount;
@@ -42,57 +43,90 @@ class WriteLog extends Command
     {
 
         // TODO
-        // ユーザー認証
+
         // 差分がない場合の処理
-        // array_diffの高速化
-
-
-        // logger()->info('This is WriteLog Command.');
 
         // ユーザー情報によって判定できるようにする
         // 基本はauto_mode 0 で停止状態
 
+        // アクセスキーを取得
+        $config = config('twitter');
+        $key = $config['api_key'];
+        $secret_key = $config['secret_key'];
+
+        // DBに保存されているaccountsの全てのtwitter_idを取得し配列に変換
+        $accounts_twitter_id = DB::table('twitter_accounts')->pluck('twitter_id');
+        $accounts_twitter_id = json_decode($accounts_twitter_id);
+
         // auto_modeが1のユーザーを取得
-        // $auto_mode_user = User::where('auto_mode', 1)->get();
+        $auto_mode_users = User::where('auto_mode', 1)->get();
 
-        // テストとして自分のアカウントを呼び出す
-        $auto_mode_user = User::find(13);
-
-        // dd($auto_mode_user);
-
-        $auto_mode = $auto_mode_user->auto_mode;
-        // dd($auto_mode);
-        // auto_modeが0か1かで処理を変える
-
-        if ($auto_mode === 1) {
-            // echo '自動フォローします' . "\n";
-            // logger()->info('自動フォローします');
-
-
-            // 自分がフォローしている人のtwitter_idを取得
-            $friends_id = \Twitter::get('friends/ids');
-
-            // DBに保存されているaccountsの全てのtwitter_idを取得し配列に変換
-            $accounts_twitter_id = DB::table('twitter_accounts')->pluck('twitter_id');
-            $accounts_twitter_id = json_decode($accounts_twitter_id);
-
-            // まずは共通項を取得
-            $common_terms = array_intersect($friends_id->ids, $accounts_twitter_id);
-
-            // dd($common_terms);
-
-            // 次にDB内accountとの差分を取得
-            $diff = array_diff($accounts_twitter_id, $common_terms);
-
-            // 差分からランダムに１つidを取得する
-            $follow_id = $diff[array_rand($diff)];
-
-            $follow = \Twitter::post('friendships/create', array('user_id' => $follow_id));
-
-            dd($follow);
+        // auto_modeが１のユーザーがいるか判定
+        if ($auto_mode_users->isEmpty()) {
+            echo '自動フォロー機能停止中です' . "\n";
         } else {
-            // echo '自動フォロー機能停止中です' . "\n";
-            logger()->info('自動フォロー機能停止中です');
+            echo '自動フォローします' . "\n";
+            foreach ($auto_mode_users as $auto_mode_user) {
+                // twitterUserを呼び出す
+                $twitter_user = $auto_mode_user->twitterUser;
+                // アクセストークンを取得
+                $token = $twitter_user->token;
+                $token_secret = $twitter_user->tokenSecret;
+
+                // APIに接続
+                $connection = new TwitterOAuth($key, $secret_key, $token, $token_secret);
+
+                // フォローしている人のtwitter_idを取得
+                $frinend_ids = $connection->get('friends/ids');
+
+                // まずは共通項を取得
+                $common_terms = array_intersect($frinend_ids->ids, $accounts_twitter_id);
+
+                // 次にDB内accountとの差分を取得
+                $diff = array_diff($accounts_twitter_id, $common_terms);
+
+                // 差分からランダムに１つidを取得する
+                $follow_id = $diff[array_rand($diff)];
+
+                $follow = $connection->post('friendships/create', array('user_id' => $follow_id));
+            }
         }
+        // $twitter_users = $auto_mode_users->twitterUser->get();
+        // dd($auto_mode_users);
+
+        // $auto_mode = $auto_mode_users->auto_mode;
+        // // dd($auto_mode);
+        // // auto_modeが0か1かで処理を変える
+
+        // if ($auto_mode === 1) {
+        //     // echo '自動フォローします' . "\n";
+        //     // logger()->info('自動フォローします');
+
+
+        //     // 自分がフォローしている人のtwitter_idを取得
+        //     $friends_id = \Twitter::get('friends/ids');
+
+        //     // DBに保存されているaccountsの全てのtwitter_idを取得し配列に変換
+        //     $accounts_twitter_id = DB::table('twitter_accounts')->pluck('twitter_id');
+        //     $accounts_twitter_id = json_decode($accounts_twitter_id);
+
+        //     // まずは共通項を取得
+        //     $common_terms = array_intersect($friends_id->ids, $accounts_twitter_id);
+
+        //     // dd($common_terms);
+
+        //     // 次にDB内accountとの差分を取得
+        //     $diff = array_diff($accounts_twitter_id, $common_terms);
+
+        //     // 差分からランダムに１つidを取得する
+        //     $follow_id = $diff[array_rand($diff)];
+
+        //     $follow = \Twitter::post('friendships/create', array('user_id' => $follow_id));
+
+        //     dd($follow);
+        // } else {
+        //     // echo '自動フォロー機能停止中です' . "\n";
+        //     logger()->info('自動フォロー機能停止中です');
+        // }
     }
 }
