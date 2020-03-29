cae23fb0277da994623bb4a857c6476329311134
hara <d06.harada.prg@gmail.com>
2020-03-29 22:12:23 +0900

    パスワード変更画面作成

--- resources/views/passEdit.blade.php
+++ resources/views/passEdit.blade.php
@@ -1,145 +1,83 @@
-{{-- <!DOCTYPE html>
-<html lang="en">
+@extends('layouts.layout')
 
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>Document</title>
-</head>
-
-<body>
-  test
-  @if (session('oauth_error'))
-  {{ session('oauth_error') }}
-@endif
-</body>
-
-</html> --}}
-
-{{-- @extends('layouts.app')
+@section('title', 'パスワード変更画面')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-</div>
-@endif
+<main>
 
-You are logged in!
-</div>
-</div>
-</div>
-</div>
-</div>
-@endsection --}}
+  {{-- form --}}
+  <div class="p-login__container">
+    <div class="p-login">
+      <p class="p-login__title">パスワード画面</p>
+      {{-- login form --}}
+      <form action="{{ route('login') }}" method="POST" class="p-login__form">
+        @csrf
 
+        <fieldset class="p-login__form-fieldset">
 
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>Document</title>
-  <link rel="stylesheet" href="{{ asset('css/style.css') }}">
-  {{-- fontawesome --}}
-  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.1.0/css/all.css"
-    integrity="sha384-lKuwvrZot6UHsBSfcMvOkWwlCMgc0TaWr+30HWe3a4ltaBwTZhyTEggF5tJv8tbt" crossorigin="anonymous">
-</head>
-
-<body>
-  {{-- header --}}
-  <header class="l-header js-l-header">
-    <a href="l-header__logo">
-      <h1 class="l-header__title">responsive practice</h1>
-    </a>
-
-
-    <!-- ハンバーガーメニュー（スマートフォン) -->
-    <div class="menu-triger js-toggle-sp-menu">
-      <span></span>
-      <span></span>
-      <span></span>
-    </div>
-
-    <nav class="p-nav nav-menu js-toggle-sp-menu-target">
-      <ul class="p-nav__menu">
-        <li class="p-nav__menu-item"><a class="p-nav__menu-link" href="">Ranking</a></li>
-        <li class="p-nav__menu-item"><a class="p-nav__menu-link" href="">Account</a></li>
-        <li class="p-nav__menu-item"><a class="p-nav__menu-link" href="">News</a></li>
-        <li class="p-nav__menu-item"><a class="p-nav__menu-link" href="">Mypage</a></li>
-        <li class="p-nav__menu-item"><a class="p-nav__menu-link" href="">Logout</a></li>
-      </ul>
-    </nav>
-
-  </header>
-  <div class="l-main">
-    <main>
-      <p class="l-main__title">パスワード変更</p>
-      <div class="p-mypage">
-
-        {{-- 左コンテンツ --}}
-        <div class="p-mypage__left">
-
-          {{-- passremind form --}}
-          <form action="#" method="POST" class="p-passremind__form">
-
-            <fieldset class="p-passremind__form-fieldset">
-
-              <p><label for="password">古いパスワード</label></p>
-              <p><input type="password" placeholder="古いパスワード"></<input>
-              </p>
-
-              <p><label for="password">新しいパスワード</label></p>
-              <p><input type="password" placeholder="新しいパスワード"></<input>
-              </p>
-
-              <p><label for="password">新しいパスワード（再入力）</label></p>
-              <p><input type="password" placeholder="新しいパスワード（再入力）"></<input>
-              </p>
-
-              <p><input type="submit" value="変更する"></<input>
-              </p>
-
-            </fieldset>
-
-          </form>
-        </div>
-
-        {{-- 右コンテンツ --}}
-        <div class="p-mypage__right">
-          <div class="p-mypage__menu">
-            <ul>
-              <li><a href="">プロフィール編集</a></li>
-              <li><a href="">パスワード変更</a></li>
-              <li><a href="">退会</a></li>
-            </ul>
+          {{-- password --}}
+          <div class="p-login__form-item">
+            <p><label for="password">古いパスワード</label></p>
+            <input type="password" id="password" class="use_icon @error('password') is-invalid @enderror"
+              name="password" required autocomplete="password" autofocus placeholder="&#xf084; 8文字以上で入力してください">
+            </<input>
+            @error('password')
+            <span class="invalid-feedback" role="alert">
+              <strong>{{ $message }}</strong>
+            </span>
+            @enderror
           </div>
 
-        </div>
+          {{-- password --}}
+          <div class="p-login__form-item">
+            <p><label for="password">新しいパスワード</label></p>
+            <input type="password" id="password" class="use_icon @error('password') is-invalid @enderror"
+              name="password" required autocomplete="password" autofocus placeholder="&#xf084; 8文字以上で入力してください">
+            </<input>
+            @error('password')
+            <span class="invalid-feedback" role="alert">
+              <strong>{{ $message }}</strong>
+            </span>
+            @enderror
+          </div>
 
+          {{-- password-re --}}
+          <div class="p-login__form-item">
+            <p><label for="password-confirmation">新しいパスワード(再入力)</label></p>
+            <input type="password" id="password-confirm"
+              class="use_icon @error('password-confirm') is-invalid @enderror" name="password_confirmation" required
+              autocomplete="password-confirm" autofocus placeholder="&#xf084;">
+            </<input>
 
-      </div>
+          </div>
 
+          <div><button type="submit">変更する</button></div>
+        </fieldset>
 
-    </main>
+      </form>
+    </div>
   </div>
 
-  <footer class="l-footer">
-    <p class="c-footer__text">Copyright © crypto-trend. All Rights Reserved</p>
-  </footer>
-  <script src="https://code.jquery.com/jquery-3.3.1.min.js"
-    integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
-  <script src="./js/app.js"></script>
-</body>
 
-</html>
+  <div class="p-mypage">
+    {{-- 後でここにformを入れる --}}
+    <div class="p-mypage__menu">
+
+      <ul>
+        <li><a href="{{ url('mypage') }}">マイページ</a></li>
+        <li><a href="{{ url('passedit') }}">パスワード変更</a></li>
+        <li><a href="{{ url('withdraw') }}">退会</a></li>
+      </ul>
+
+    </div>
+
+  </div>
+
+
+  </div>
+
+
+</main>
+
+@endsection
