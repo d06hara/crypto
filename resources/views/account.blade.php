81ccaf4fe4010e891b5d05446395251f655e3463
hara <d06.harada.prg@gmail.com>
2020-03-30 21:58:23 +0900

    アカウント一覧裏画面 css修正　レスポンシブ化

--- resources/views/account.blade.php
+++ resources/views/account.blade.php
@@ -43,12 +43,16 @@
 @else
 
 <main>
-  <p>この機能を利用するにはtwitterアカウント登録が必要です</p>
-  <div>
-    <a href="{{ url('/twitter') }}">
-      <i class="fab fa-twitter"></i><span>twitterアカウントを登録する</span>
-    </a>
+  <div class="p-accountauth">
+    <p class="p-accountauth__text">この機能を利用するにはtwitterアカウント登録が必要です。</p>
+    <p class="p-accountauth__text">ご利用中のtwitterアカウントを連携する方は以下のボタンを押してください。</p>
+    <div class="p-accountauth__btn">
+      <a href="{{ url('/twitter') }}" class="u-twitterbtn">
+        <i class="fab fa-twitter"></i><span>twitterアカウントを登録する</span>
+      </a>
+    </div>
   </div>
+
 </main>
 
 @endif
