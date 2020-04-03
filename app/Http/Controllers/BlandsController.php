02f81a02b0cd6385ef450f542f317cfe494700d2
hara <d06.harada.prg@gmail.com>
2020-04-04 04:19:13 +0900

    BlandsController修正

--- app/Http/Controllers/BlandsController.php
+++ app/Http/Controllers/BlandsController.php
@@ -7,13 +7,4 @@ use Illuminate\Http\Request;
 
 class BlandsController extends Controller
 {
-    /**
-     * 銘柄の削除
-     */
-    public function delete($id)
-    {
-        Bland::find($id)->delete(); //softDelete
-
-        return redirect()->to('/login');
-    }
 }
