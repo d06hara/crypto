cb21054ec36fd4344803a3eddb7c4f92b33ab47d
hara <d06.harada.prg@gmail.com>
2020-04-04 14:31:11 +0900

    email.blade 修正

--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -1,52 +1,3 @@
-{{-- @extends('layouts.app')
-
-@section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Reset Password') }}</div>
-
-<div class="card-body">
-    @if (session('status'))
-    <div class="alert alert-success" role="alert">
-        {{ session('status') }}
-    </div>
-    @endif
-
-    <form method="POST" action="{{ route('password.email') }}">
-        @csrf
-
-        <div class="form-group row">
-            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
-
-            <div class="col-md-6">
-                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email"
-                    value="{{ old('email') }}" required autocomplete="email" autofocus>
-
-                @error('email')
-                <span class="invalid-feedback" role="alert">
-                    <strong>{{ $message }}</strong>
-                </span>
-                @enderror
-            </div>
-        </div>
-
-        <div class="form-group row mb-0">
-            <div class="col-md-6 offset-md-4">
-                <button type="submit" class="btn btn-primary">
-                    {{ __('Send Password Reset Link') }}
-                </button>
-            </div>
-        </div>
-    </form>
-</div>
-</div>
-</div>
-</div>
-</div>
-@endsection --}}
-
 @extends('layouts.layout')
 @section('content')
 <main>
@@ -57,20 +8,18 @@
             <div class="c-form">
 
                 <p class="c-form__title"><span class="c-form__title-accent"></span>パスワードリマインダー</p>
-                {{-- テンプレートにあったので後で修正して加える --}}
+
                 @if (session('status'))
-                <div class="alert alert-success" role="alert">
-                    {{ session('status') }}
+                <div class="c-flash">
+                    <div class="c-flash__error" role="alert">
+                        {{ session('status') }}
+                    </div>
                 </div>
                 @endif
 
-
-                {{-- passremind form --}}
                 <form action="{{ route('password.email') }}" method="POST" class="c-form__contents">
                     @csrf
-
                     <fieldset class="c-form__contents-fieldset">
-
                         <div class="c-form__contents-item">
                             <p><label for="email">E-mail address</label></p>
 
@@ -78,7 +27,6 @@
                                 class="use_icon form-control @error('email') is-invalid @enderror" name="email"
                                 value="{{ old('email') }}" required autocomplete="email" autofocus
                                 placeholder="&#xf0e0;">
-
                             @error('email')
                             <span class="invalid-feedback" role="alert">
                                 <strong>{{ $message }}</strong>
@@ -89,9 +37,7 @@
 
                         </div>
                     </fieldset>
-
                 </form>
-
             </div>
         </div>
 
