fbbf3e83be171096b56dde6ce87a8a44d92dbf8d
hara <d06.harada.prg@gmail.com>
2020-04-04 14:15:26 +0900

    BlandPanelコメント整理

--- resources/js/components/BlandPanel.vue
+++ resources/js/components/BlandPanel.vue
@@ -57,7 +57,6 @@
             </tr>
           </thead>
           <tbody class="c-table__tbody">
-            <!-- <tr v-for="(bland, index) in blands" :key="bland.id"> -->
             <tr v-for="(bland, index) in selectedBlands" :key="bland.id">
               <td width="10">
                 <a v-bind:href="bland.url" target="_blank">{{ index + 1 }}</a>
@@ -82,14 +81,10 @@
 <script>
 export default {
   name: "BlandPanel",
-
-  // props: ["time"],
   data() {
     return {
-      selected: null,
-      // options: ["list", "of", "options"],
       check_lists: [
-        //絞り込み機能用に銘柄を格納
+        //filter機能用に銘柄を格納
         "ビットコイン",
         "イーサリアム",
         "イーサリアムクラシック",
@@ -104,30 +99,17 @@ export default {
         "クアンタム"
       ],
       preview: [], //絞り込んだ銘柄を格納するため空配列
-      blands: [],
-      time: {}, //DB最終更新時間を入れる
-      // sortItems: [
-      //   { name: "過去1時間" },
-      //   { name: "過去1日" },
-      //   { name: "過去1週間" }
-      // ],
-      sortKey: "",
-      activetab: 1
-      // test: {
-      //   key: "", //ソートキー
-      //   mode: false
-      // }
+      blands: [], //銘柄情報入れるための空配列
+      time: [], //DB最終更新時間を入れる
+      sortKey: "", //ソートキー
+      activetab: 1 //ソートの状態表示
     };
   },
-  // created: function() {
-  //   this.sort("sort");
-  // },
+
   mounted: function() {
+    console.log("mounted");
     axios
       .get("/api/ranking")
-      // .then(response => console.log(response))
-      // .then(response => (this.time = response.data[0]))
-      // .then(response => (this.blands = response.data[1]))
       .then(response => {
         // 時間を取得
         this.time = response.data[0];
@@ -139,68 +121,43 @@ export default {
     this.sortBy("hour");
   },
   computed: {
-    // checkboxを使用した銘柄filter
+    /**
+     * sort&fileter
+     */
     selectedBlands: function() {
-      // var data = this.blands;
+      // sortKeyでswitch
+      switch (this.sortKey) {
+        case "hour":
+          console.log("１時間で並び替え");
+          this.activetab = 1;
+          this.blands.sort(function(a, b) {
+            if (a.hour_tweets_count > b.hour_tweets_count) return -1;
+            if (a.hour_tweets_count < b.hour_tweets_count) return 1;
+            return 0;
+          });
+          break;
 
-      // var sortKey = this.sortKey;
+        case "day":
+          console.log("１日で並び替え");
+          this.activetab = 2;
+          this.blands.sort(function(a, b) {
+            if (a.day_tweets_count > b.day_tweets_count) return -1;
+            if (a.day_tweets_count < b.day_tweets_count) return 1;
+            return 0;
+          });
+          break;
 
-      // sortkeyの有無で処理を分ける
-      // sortKeyがある場合⇨sortボタンが押された場合
-      if (this.sortKey) {
-        // -----------------
-        // sort処理
-        // -----------------
-        switch (this.sortKey) {
-          case "hour":
-            console.log("１時間で並び替え");
-            this.activetab = 1;
-            this.blands.sort(function(a, b) {
-              if (a.hour_tweets_count > b.hour_tweets_count) return -1;
-              if (a.hour_tweets_count < b.hour_tweets_count) return 1;
-              return 0;
-            });
-            break;
-          case "day":
-            console.log("１日で並び替え");
-            this.activetab = 2;
-            this.blands.sort(function(a, b) {
-              if (a.day_tweets_count > b.day_tweets_count) return -1;
-              if (a.day_tweets_count < b.day_tweets_count) return 1;
-              return 0;
-            });
-            break;
-          case "week":
-            console.log("１週間で並び替え");
-            this.activetab = 3;
-            this.blands.sort(function(a, b) {
-              if (a.week_tweets_count > b.week_tweets_count) return -1;
-              if (a.week_tweets_count < b.week_tweets_count) return 1;
-              return 0;
-            });
-            break;
-          default:
-        }
-        // 初期値,previewに何もない場合は全てを表示
-        if (this.preview.length === 0) {
-          return this.blands;
-        }
-        // previewに入っている場合、その名前を含んでいる銘柄を取得
-        return this.blands.filter(function(bland) {
-          return this.preview.includes(bland.name);
-        }, this);
+        case "week":
+          console.log("１週間で並び替え");
+          this.activetab = 3;
+          this.blands.sort(function(a, b) {
+            if (a.week_tweets_count > b.week_tweets_count) return -1;
+            if (a.week_tweets_count < b.week_tweets_count) return 1;
+            return 0;
+          });
+          break;
+        default:
       }
-      // sortKeyがない場合=初期表示では1時間のツイート数順に並べる
-      console.log("初期表示です、１時間で並べます");
-      this.sortKey = "hour";
-
-      console.log("１時間で並び替え");
-      this.blands.sort(function(a, b) {
-        if (a.hour_tweets_count > b.hour_tweets_count) return -1;
-        if (a.hour_tweets_count < b.hour_tweets_count) return 1;
-        return 0;
-      });
-
       // 初期値,previewに何もない場合は全てを表示
       if (this.preview.length === 0) {
         return this.blands;
@@ -211,38 +168,14 @@ export default {
       }, this);
     }
   },
-  // ソート機能発火
+  /**
+   * ソート機能発火
+   */
   methods: {
     sortBy: function(key) {
+      console.log("methods");
       this.sortKey = key;
     }
   }
-
-  // methods: {
-  //   find_blands: function() {
-  //     var blands = this.blands;
-  //     var preview = this.preview;
-
-  //     if (preview.length > 0) {
-  //       for (var i = 0; i < blands.length; i++) {
-  //         var check_bland = blands[i].name;
-  //         for (var j = 0; j < preview.length; j++) {
-  //           if (check_bland === preview[j]) {
-  //             blands[i].display = true;
-  //             break;
-  //           } else {
-  //             blands[i].display = false;
-  //           }
-  //         }
-  //       }
-  //       // checkが全て外れた場合の処理
-  //     } else {
-  //       for (var i = 0; i < blands.length; i++) {
-  //         // var checked_bland = blands[i].name;
-  //         blands[i].display = true;
-  //       }
-  //     }
-  //   }
-  // },
 };
 </script>
