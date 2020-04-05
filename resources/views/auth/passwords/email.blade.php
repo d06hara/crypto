134ac9028372799e47f0c3e93021ebaf3d01964c
vagrant <vagrant@homestead>
2020-04-05 16:00:28 +0000

    email.blade

--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -9,37 +9,36 @@
 
                 <p class="c-form__title"><span class="c-form__title-accent"></span>パスワードリマインダー</p>
 
-                @if (session('status'))
+                {{-- @if (session('status'))
                 <div class="c-flash">
                     <div class="c-flash__error" role="alert">
                         {{ session('status') }}
-                    </div>
-                </div>
-                @endif
-
-                <form action="{{ route('password.email') }}" method="POST" class="c-form__contents">
-                    @csrf
-                    <fieldset class="c-form__contents-fieldset">
-                        <div class="c-form__contents-item">
-                            <p><label for="email">E-mail address</label></p>
-
-                            <input id="email" type="email"
-                                class="use_icon form-control @error('email') is-invalid @enderror" name="email"
-                                value="{{ old('email') }}" required autocomplete="email" autofocus
-                                placeholder="&#xf0e0;">
-                            @error('email')
-                            <span class="invalid-feedback" role="alert">
-                                <strong>{{ $message }}</strong>
-                            </span>
-                            @enderror
-
-                            <div><button type="submit">送信</button></div>
-
-                        </div>
-                    </fieldset>
-                </form>
             </div>
         </div>
+        @endif --}}
+
+        <form action="{{ route('password.email') }}" method="POST" class="c-form__contents">
+            @csrf
+            <fieldset class="c-form__contents-fieldset">
+                <div class="c-form__contents-item">
+                    <p><label for="email">E-mail address</label></p>
+
+                    <input id="email" type="email" class="use_icon form-control @error('email') is-invalid @enderror"
+                        name="email" value="{{ old('email') }}" required autocomplete="email" autofocus
+                        placeholder="&#xf0e0;">
+                    @error('email')
+                    <span class="invalid-feedback" role="alert">
+                        <strong>{{ $message }}</strong>
+                    </span>
+                    @enderror
+
+                    <div><button type="submit">送信</button></div>
+
+                </div>
+            </fieldset>
+        </form>
+    </div>
+    </div>
 
 
 </main>
